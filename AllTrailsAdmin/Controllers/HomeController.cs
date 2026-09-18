782d103ea47afd4a5b67b2c97e1fd99bf500ad1c
Michael Trumbull <michael.trumbull@nscc.ca>
2026-09-18 14:31:13 -0300

    Working with controllers and routing.

--- AllTrailsAdmin/Controllers/HomeController.cs
+++ AllTrailsAdmin/Controllers/HomeController.cs
@@ -6,15 +6,48 @@ namespace AllTrailsAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        // Constructor
+        public HomeController()
+        {
+            Console.WriteLine("Constructor called.");
+        }
+
+        //
+        // Action Methods
+        //
+
+        // GET: Home/Index
         public IActionResult Index()
         {
+            Console.WriteLine("/Home/Index action method called.");
+
+            return View(); // /View/Home/Index view
+        }
+
+        // GET: Home/Privacy
+        public IActionResult Privacy()
+        {
+            Console.WriteLine("/Home/Privacy action method called.");
+
+            return View(); // /View/Home/Privacy view
+        }
+
+
+        public IActionResult MyFirstActionMethod()
+        {
+            Console.WriteLine("MyFirstActionMethod is called.");
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
+
+
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
