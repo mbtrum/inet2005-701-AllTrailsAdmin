ab5717863f3b2e23109cad950844755815eac7a7
Michael Trumbull <michael.trumbull@nscc.ca>
2026-09-21 11:30:02 -0300

    Starting to define Trail model.

---
+++ AllTrailsAdmin/Models/Trail.cs
@@ -0,0 +1,15 @@
+﻿namespace AllTrailsAdmin.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+                
+        public int Length { get; set; } // metres 1000m 
+
+        public int ElevationGain { get; set; } // metres 1000m
+    }
+}
